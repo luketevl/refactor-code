6cdb2318af28bde44503aa3440a7e3f3df7fc719
luketevl <luketevl@gmail.com>
2016-12-07 15:44:24 -0200

    initial commit REFACTORED ;)

---
+++ exercicio/js/internacoes.js
@@ -0,0 +1,125 @@
+var pac =[
+    {
+        "numero": "1", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": false   
+    },
+    
+    {
+        "numero": "2", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": true   
+    },
+    
+    {
+        "numero": "3", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": false   
+    },
+    
+    {
+        "numero": "4", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": true   
+    },
+    
+    {
+        "numero": "5", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": false   
+    },
+    
+    {
+        "numero": "6", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": true   
+    },
+    
+    {
+        "numero": "7", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": true   
+    },
+    
+    {
+        "numero": "8", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": false   
+    },
+    
+    {
+        "numero": "9", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": true   
+    },
+    
+    {
+        "numero": "10", 
+        "nome": "Ronald Jones",
+        "cid": "17063",
+        "complemento": [{
+            "endereco": "0 Ohio Court",
+            "bairro": "Center",
+            "cidade": "New Jersey"
+        },],
+        "convenio": false   
+    },
+
+    
+    
+    
+]
